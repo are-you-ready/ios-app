fb80cbf96cb9e53cb3a4cc2c4ae62162c7add93c
Markus Tran <thebluepillow@gmail.com>
2017-05-17 10:47:36 -0700

    Initial Commit

---
+++ Are You Ready?/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Are You Ready?
+//
+//  Created by Markus Tran on 5/17/17.
+//  Copyright © 2017 Markus Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
