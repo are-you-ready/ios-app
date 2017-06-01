dad334e43b29efdb277adaf20b23ab6d0940673c
Markus Tran <thebluepillow@gmail.com>
2017-05-31 19:05:05 -0700

    redesign API for ease of use

---
+++ Are You Ready?/AYRUserProfile.swift
@@ -0,0 +1,13 @@
+//
+//  AYRUserProfile.swift
+//  Are You Ready?
+//
+//  Created by Markus Tran on 5/18/17.
+//  Copyright © 2017 Markus Tran. All rights reserved.
+//
+
+import UIKit
+
+class AYRUserProfile: NSObject {
+
+}
