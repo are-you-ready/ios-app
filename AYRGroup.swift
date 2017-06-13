0b282856ccea695637b039d135d9612bd410b027
Markus Tran <thebluepillow@gmail.com>
2017-06-12 19:30:49 -0700

    temp

---
+++ AYRGroup.swift
@@ -0,0 +1,9 @@
+//
+//  AYRGroup.swift
+//  Are You Ready?
+//
+//  Created by Markus Tran on 6/5/17.
+//  Copyright © 2017 Markus Tran. All rights reserved.
+//
+
+import Foundation
---
+++ AYRUser.swift
@@ -0,0 +1,9 @@
+//
+//  AYRUser.swift
+//  Are You Ready?
+//
+//  Created by Markus Tran on 6/5/17.
+//  Copyright © 2017 Markus Tran. All rights reserved.
+//
+
+import Foundation
