cc3da95ab52a297add9cdf5c31f0472d0a5d4fc3
Markus Tran <thebluepillow@gmail.com>
2017-06-15 18:30:15 -0700

    easy API

--- AYRGroup.swift
+++ AYRGroup.swift
@@ -6,34 +6,48 @@
 //  Copyright © 2017 Markus Tran. All rights reserved.
 //
 
+import Foundation
+
 class AYRGroup {
     let name: String
-    let users: [String: AYRPartialUser]
-//    let events: [String: AYREvent]
+    let users: [String: AYRUser]
+    let events: [String: AYREvent]
     
-    init(name: String, users: [String: AYRPartialUser]) {
+    init(name: String, users: [String: AYRUser], events: [String: AYREvent]) {
         self.name = name
         self.users = users
+        self.events = events
     }
     
     init?(fromJSON json: Any?) {
         guard let json = json as? [String: Any],
-              let name = json["name"] as? String,
-              let users = json["users"] as? [Any]
+              let jsonName = json["name"] as? String,
+              let jsonUsers = json["users"] as? [Any],
+              let jsonEvents = json["events"] as? [Any]
         else {
             return nil
         }
-        
-        var parsedUsers = [String: AYRPartialUser]()
-        for user in users {
-            if let parsedUser = AYRPartialUser(fromJSON: user) {
-                parsedUsers[parsedUser.name] = parsedUser
+
+        var users = [String: AYRUser]()
+        for jsonUser in jsonUsers {
+            if let user = AYRUser(fromJSON: jsonUser) {
+                users[user.name] = user
             } else {
                 return nil
             }
         }
-        
-        self.name = name
-        self.users = parsedUsers
+
+        var events = [String: AYREvent]()
+        for jsonEvent in jsonEvents {
+            if let event = AYREvent(fromJSON: jsonEvent) {
+                events[event.name] = event
+            } else {
+                return nil
+            }
+        }
+
+        self.name = jsonName
+        self.users = users
+        self.events = events
     }
 }
