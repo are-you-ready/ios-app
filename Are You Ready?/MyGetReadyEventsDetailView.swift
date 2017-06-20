571979f7b9ac08f646eceb26bbf493f37c9488cf
Markus Tran <thebluepillow@gmail.com>
2017-06-20 10:34:02 -0700

    wtf is this

--- Are You Ready?/MyGetReadyEventsDetailView.swift
+++ Are You Ready?/MyGetReadyEventsDetailView.swift
@@ -22,7 +22,6 @@ class MyGetReadyEventsDetailView: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
