139540cd49f7dcdd561354f98a6136d1a3815a35
Markus Tran <thebluepillow@gmail.com>
2017-06-19 22:28:50 -0700

    two detail views

--- Are You Ready?/MyEventsTableViewController.swift
+++ Are You Ready?/MyEventsTableViewController.swift
@@ -10,20 +10,38 @@ import UIKit
 
 class MyEventsTableViewController: UITableViewController {
 
-    var myEvents = [AYREvent]()
+    var myToRespondEvents = [AYREvent]()
+    var myToGetReadyEvents = [AYREvent]()
     var eventIndex = 0
     
     func handleRefresh() {
         AreYouReadyAPI.getGroup("cis55") { result in
             switch (result) {
             case let .success(group):
-                self.myEvents = Array(group.events.values)
+                let myEvents = Array(group.events.values)
+                self.myToRespondEvents = []
+                self.myToGetReadyEvents = []
+                
+                for event in myEvents {
+                    var hasNotResponded = false
+                    for attendee in Array(event.attendees.values) {
+                        if attendee.status == .pending {
+                            hasNotResponded = true
+                        }
+                    }
+                    if hasNotResponded == true {
+                        self.myToRespondEvents += [event]
+                    } else {
+                        self.myToGetReadyEvents += [event]
+                    }
+                }
                 self.tableView.reloadData()
             case let .failure(.requestFailure(reason, _)),
                  let .failure(.JSONParseFailure(reason)),
                  let .failure(.JSONErrorResponse(reason, _)):
                 print("Request failed because: \(reason)")
-                self.myEvents = []
+                self.myToRespondEvents = []
+                self.myToGetReadyEvents = []
             }
             
             self.refreshControl?.endRefreshing()
@@ -44,31 +62,75 @@ class MyEventsTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return myEvents.count
+        switch section {
+        case 0: return myToRespondEvents.count
+        case 1: return myToGetReadyEvents.count
+        default: return 0
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MyEventsCell", for: indexPath) as! MyEventsTableViewCell
-        let event = myEvents[indexPath.row]
-        
-        cell.cellEventName.text = event.name
-        cell.cellEventTime.text = event.readyTime.description
-        cell.cellEventLocation.text = event.location
+        switch indexPath.section {
+        case 0:
 
-        eventIndex = indexPath.row
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MyEventsCell", for: indexPath) as! MyEventsTableViewCell
+            let event = myToRespondEvents[indexPath.row]
         
-        return cell
+            cell.cellEventName.text = event.name
+            cell.cellEventTime.text = event.readyTime.description
+            cell.cellEventLocation.text = event.location
+
+            eventIndex = indexPath.row
+        
+            return cell
+            
+        case 1:
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MyGetReadyEventCell", for: indexPath) as! MyGetReadyEventsTableViewCell
+            let event = myToGetReadyEvents[indexPath.row]
+            
+            cell.cellEventName.text = event.name
+            cell.cellEventTime.text = event.readyTime.description
+            cell.cellEventLocation.text = event.location
+            
+            return cell
+            
+        default:
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MyEventsCell", for: indexPath) as! MyEventsTableViewCell
+            let event = myToRespondEvents[indexPath.row]
+            
+            cell.cellEventName.text = event.name
+            cell.cellEventTime.text = event.readyTime.description
+            cell.cellEventLocation.text = event.location
+            
+            eventIndex = indexPath.row
+            
+            return cell
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0: return "Are you coming?"
+        case 1: return "Are you ready?"
+        default: return nil
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "MyEventsDetailSegue" {
             let viewController = segue.destination as! MyEventsDetailViewController
             let cell = sender as! MyEventsTableViewCell
-            viewController.myEvent = myEvents[self.tableView.indexPath(for: cell)!.row]
+            viewController.myEvent = myToRespondEvents[self.tableView.indexPath(for: cell)!.row]
+        } else if segue.identifier == "MyToGetReadyEventsDetailSegue" {
+            let viewController = segue.destination as! MyGetReadyEventsDetailView
+            let cell = sender as! MyGetReadyEventsTableViewCell
+            viewController.myEvent = myToGetReadyEvents[self.tableView.indexPath(for: cell)!.row]
         }
     }
 }
