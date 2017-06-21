ec4a9a66e46d0588ff7556a60d6ad3d8502c8139
Ryan Thomas Hobbs <023906973@atc203-m13.local>
2017-06-20 19:40:42 -0700

    do it

--- Are You Ready?/MyGetReadyEventsDetailViewController.swift
+++ Are You Ready?/MyGetReadyEventsDetailViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class MyGetReadyEventsDetailViewController: UIViewController {
 
+    var timer: Timer? = nil
     var myEvent: AYREvent? = nil
+    var seconds_left: TimeInterval = 1
     
     @IBOutlet weak var eventName: UILabel!
     @IBOutlet weak var whosReady: UITextView!
-
+    @IBOutlet weak var timer_label: UILabel!
+    
     @IBAction func pressReady(_ sender: Any) {
         AreYouReadyAPI.updateStatus(group: "cis55", event: myEvent!.name, user: "Markus", status: .ready) { result in
             switch (result) {
@@ -30,7 +33,7 @@ class MyGetReadyEventsDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         eventName.text = myEvent?.name
         
         var whosReadyText = ""
@@ -43,6 +46,13 @@ class MyGetReadyEventsDetailViewController: UIViewController {
             }
         }
         whosReady.text = whosReadyText
+        
+        let notificationTime = myEvent!.readyTime
+        seconds_left = notificationTime.timeIntervalSince(Date())
+        if (seconds_left >= 0) {
+            runTimer()
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -50,6 +60,27 @@ class MyGetReadyEventsDetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func timeString(time: TimeInterval) -> String {
+        let hours = Int(time) / 3600
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format: "%02i:%02i:%02i", hours, minutes, seconds)
+    }
+    
+    func updateTimer() {
+        seconds_left -= 1
+        if seconds_left > 0 {
+            timer_label.text = timeString(time: seconds_left)
+        } else {
+            timer_label.text = "00:00:00"
+            timer!.invalidate()
+        }
+    }
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(MyGetReadyEventsDetailViewController.updateTimer)), userInfo: nil, repeats: true)
+    }
+    
     /*
     // MARK: - Navigation
 
